f7847e815ba41369c8c4af68c556c354f4c6bce0
ChikaEche <chikaokafor94@gmail.com>
2023-07-07 18:45:10 +0100

    client errors

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,8 @@ use axum::{
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
-    EmptyArr,
     EntryNotFound { id: u64 },
 }
 
@@ -15,6 +14,30 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         print!("-->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "CLIENT_ERROR").into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Self::EntryNotFound { .. } => (StatusCode::BAD_REQUEST, ClientError::EntryNotFound),
+
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    EntryNotFound,
+    SERVICE_ERROR,
 }
